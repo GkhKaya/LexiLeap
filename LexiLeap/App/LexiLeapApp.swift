18783317ad3965616623dd5b9c8a2eaecce2fe55
GkhKaya <gkhkaya00@gmail.com>
2024-07-11 00:50:04 +0300

    Started project and created some files

---
+++ LexiLeap/App/LexiLeapApp.swift
@@ -0,0 +1,17 @@
+//
+//  LexiLeapApp.swift
+//  LexiLeap
+//
+//  Created by Gokhan Kaya on 11.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct LexiLeapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
