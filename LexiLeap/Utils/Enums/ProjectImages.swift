e5d27f8b073f89daeeecb2749b21862baab6bb87
GkhKaya <gkhkaya00@gmail.com>
2024-07-11 11:48:50 +0300

    Onboard images and enum added

---
+++ LexiLeap/Utils/Enums/ProjectImages.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectImages.swift
+//  LexiLeap
+//
+//  Created by Gokhan Kaya on 11.07.2024.
+//
+
+import Foundation
+
+struct ProjectImages{
+    
+    enum OnboardScreen : String{
+        case onboard1 = "Onboard1"
+        case onboard2 = "Onboard2"
+        case onboard3 = "Onboard3"
+    }
+}
